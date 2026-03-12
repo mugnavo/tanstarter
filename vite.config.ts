d4eef4ab0337b730a024c651ec86bb77fe1c074a
Nathaniel John Tampus <nat@nize.ph>
2026-03-13 06:26:26 +0800

    fix: temporarily stay on @vitejs/plugin-react 5.1.4 (https://github.com/rolldown/plugins/issues/10)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import babel from "@rolldown/plugin-babel";
+// import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+// import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
@@ -18,10 +19,23 @@ export default defineConfig({
     tanstackStart(),
     // https://tanstack.com/start/latest/docs/framework/react/guide/hosting
     nitro(),
-    viteReact(),
-    // https://npmx.dev/package/@vitejs/plugin-react#user-content-react-compiler
-    babel({
-      presets: [reactCompilerPreset()],
+    // viteReact(),
+    // // https://npmx.dev/package/@vitejs/plugin-react#user-content-react-compiler
+    // babel({
+    //   presets: [reactCompilerPreset()],
+    // }),
+    // issue for @vitejs/plugin-react 6.0: https://github.com/rolldown/plugins/issues/10
+    viteReact({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-react-compiler",
+            {
+              target: "19",
+            },
+          ],
+        ],
+      },
     }),
     tailwindcss(),
   ],
