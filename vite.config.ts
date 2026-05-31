c07e1170500c07402ad7987f87885f1bc832a8b9
Raphaël Balet <raphael.balet@outlook.com>
2026-05-31 09:19:51 +0200

    build(vite.config.ts): missing packge.json version import

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
+import { version } from "./package.json";
 
 export default defineConfig({
   define: {
